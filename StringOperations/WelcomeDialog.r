79e211d87125b08e287b9125a8c27d62f6f39ed6
Adam Lukaszewicz <lukaszewicz.ad@gmail.com>
2024-02-18 19:03:32 +0100

    Updated with age column

--- StringOperations/WelcomeDialog.r
+++ StringOperations/WelcomeDialog.r
@@ -1,10 +1,11 @@
-# Function to add name to existing CSV file
+# Function to add name and age to existing CSV file
 welcome_dialog <- function() {
-  # Define a name input question:
+  # Define name and age input questions
   name <- readline("Hello World! Type your name: ")
+  age <- as.integer(readline("How old are you? "))
 
-  # Create a data frame with the name
-  data <- data.frame(name = name)
+  # Create a data frame with the name and age
+  data <- data.frame(name = name, age = age)
 
   # Check if the file already exists
   if (file.exists("names.csv")) {
@@ -17,12 +18,12 @@ welcome_dialog <- function() {
     # Write the updated data to the CSV file
     write.csv(updated_data, file = "names.csv", row.names = FALSE)
     
-    greetings <- paste("Hi ", name, "! Your name has been added to names.csv.")
+    greetings <- paste("Hi ", name, "! Your name and age have been added to names.csv.")
   } else {
     # If the file does not exist, create a new file
     write.csv(data, file = "names.csv", row.names = FALSE)
     
-    greetings <- paste("Hi ", name, "! Your name has been written to names.csv.")
+    greetings <- paste("Hi ", name, "! Your name and age have been written to names.csv.")
   }
 
   return(greetings)
